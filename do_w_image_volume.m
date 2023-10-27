bb84de855892659e32c27f80713ee59109d3a98b
Markus Nilsson <nilzzon@gmail.com>
2023-10-27 18:13:02 +0200

    Moving code

--- do_w_image_volume.m
+++ do_w_image_volume.m
@@ -9,31 +9,7 @@ classdef do_w_image_volume < do_w_image_vector
 
         end
 
-        function O = trim(O, ir, jr, kr, vr)
 
-            if (nargin < 2) || (isempty(ir)), ir = 1:O.h.dim(2); end
-            if (nargin < 3) || (isempty(jr)), jr = 1:O.h.dim(3); end
-            if (nargin < 4) || (isempty(kr)), kr = 1:O.h.dim(4); end
-            if (nargin < 5) || (isempty(vr)), vr = 1:O.h.dim(5); end
-
-            I = O.imreshape();
-            I = I(ir, jr, kr, vr);
-            O.w = reshape(I, [prod(size(I,1,2,3)) size(I,4)]);
-            O.n_vox = size(O.w, 1);
-
-            O.h.dim(2:5) = size(I, 1, 2, 3, 4);
-
-            % adjust offsets
-            ind = [min(ir)-1 min(jr)-1 min(kr)-1 1]';
-            cm = [O.h.srow_x'; O.h.srow_y'; O.h.srow_z';  0 0 0 1];
-            physc = cm * ind;
-
-            O.h.qform_code = 0;
-            O.h.srow_x(4) = physc(1);
-            O.h.srow_y(4) = physc(2);
-            O.h.srow_z(4) = physc(3);
-            
-        end
 
     end
 
@@ -58,7 +34,6 @@ classdef do_w_image_volume < do_w_image_vector
             if (sum(X(:).^2) > 1e-5)
                 error('need transverse slices for now');
             end
-
 
             I = zeros(double(h_hr.dim(2:5)'));
 
